f3e06d9d9df7eceeb54cec773d5e8ff23f6292a0
Philip Oesterlin <philip.oesterlin@gmail.com>
2019-11-20 23:15:44 +0100

    add normalisation and gps distance calculation

--- loadData.m
+++ loadData.m
@@ -1,7 +1,9 @@
 
-clear ; close all; clc
+function [X,y] = loadData()
 
+% search for all avaliable csv files
 files = dir('**/*.csv');
+
 GPS = zeros(1,2);
 ACC = zeros(1,3);
 ACCLin = zeros(1,3);
@@ -27,7 +29,23 @@ for i = 1:length(files)
         otherwise 
             fprintf('--- file ignored \n');
     end
-    
 end
 
-plot(ACC);
+% TODO: use HIGHPASS filter
+
+% normalize
+ACCLin = normalize(ACCLin,'range');
+GYR = normalize(GYR,'range');
+COMP = normalize(COMP,'range');
+
+% calc gps distance between coordinate n and n+1
+s = length(GPS);
+dist = zeros(s(:,1), 1);
+for i = 1:length(GPS)-1
+    dist(i) = distance(GPS(i, :), GPS(i+1, :));
+end
+
+y = dist;
+X = {ACCLin, GYR, COMP};
+
+end
---
+++ script.m
@@ -0,0 +1,9 @@
+
+
+clear ; close all; clc
+
+[X, y] = loadData();
+
+ACC = X{1,1};
+
+plot(ACC);
